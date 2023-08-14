ead60c82fcbc3ce7a5625309c63fee868218a87b
asnavn <asnavn@proton.me>
2023-08-14 20:12:03 +0700

    test block domain

--- cf_gateway_rule_create.js
+++ cf_gateway_rule_create.js
@@ -31,7 +31,7 @@ async function getZeroTrustLists() {
     // Build the wirefilter expression
     for (const list of filtered_lists) {
         //wirefilter_expression += `dns.fqdn in \$${list.id} or `;
-        wirefilter_expression += `any(dns.domain[*] in ${list.id}) or `;
+        wirefilter_expression += `any(domain in dns.domain for domain in ${list.id}) or `;
         //wirefilter_expression += `dns.domain in \$${list.id} or `;
     }
     // Remove the trailing ' or '
